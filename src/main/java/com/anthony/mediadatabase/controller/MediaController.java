ff08466b1803b0c581797da57ef92b5ab26693e7
Anthony Flowers <AnthonyFlowers@users.noreply.github.com>
2021-01-20 15:21:40 -0600

    Added tv shows to home page that shows all media. Added column to home page table that indicates the type of each media item

--- src/main/java/com/anthony/mediadatabase/controller/MediaController.java
+++ src/main/java/com/anthony/mediadatabase/controller/MediaController.java
@@ -1,8 +1,6 @@
 package com.anthony.mediadatabase.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +11,19 @@ import com.anthony.mediadatabase.model.User;
 import com.anthony.mediadatabase.repository.MediaRepository;
 import com.anthony.mediadatabase.repository.MovieRepository;
 import com.anthony.mediadatabase.repository.TVShowRepository;
-import com.anthony.mediadatabase.service.UserService;
 
 @Controller
-public class MediaController extends UserAuthenticatedController{
-	
+public class MediaController extends UserAuthenticatedController {
+
 	@Autowired
 	private MovieRepository movieRepository;
-	
+
 	@Autowired
 	private TVShowRepository showRepository;
-	
+
 	@Autowired
 	private MediaRepository mediaRepository;
-	
+
 	@GetMapping("/media")
 	public String mediaMain(Model model) {
 		User user = getUser();
@@ -34,7 +31,7 @@ public class MediaController extends UserAuthenticatedController{
 		model.addAttribute("tvShows", showRepository.findAllByUserId(user.getId()));
 		return "index";
 	}
-	
+
 	@GetMapping("/editMedia")
 	public String editMedia(@RequestParam(name = "selectedMedia", required = true) Integer mediaId, Model model) {
 		MediaItem selectedItem = mediaRepository.findById(mediaId);
