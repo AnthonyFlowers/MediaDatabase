1add1aba9242efca0d31b3b069c6598e5ab95741
AnthonyF <22030883+AnthonyFlowers@users.noreply.github.com>
2021-08-09 19:03:08 -0500

    Merged MainController with the MediaController.

--- src/main/java/com/anthony/mediadatabase/controller/MainController.java
+++ src/main/java/com/anthony/mediadatabase/controller/MainController.java
@@ -1,17 +1,36 @@
 package com.anthony.mediadatabase.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.anthony.mediadatabase.model.MediaItem;
+import com.anthony.mediadatabase.model.User;
+import com.anthony.mediadatabase.repository.BookRepository;
+import com.anthony.mediadatabase.repository.MediaRepository;
+import com.anthony.mediadatabase.repository.MovieRepository;
+import com.anthony.mediadatabase.repository.TVShowRepository;
 
 @Controller
-public class MainController {
-	@SuppressWarnings("unused")
-	private static final Logger Log = LoggerFactory.getLogger(MainController.class);
+public class MediaController extends UserAuthenticatedController {
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+	@Autowired
+	private TVShowRepository showRepository;
 	
-	@GetMapping("/")
-	public String main() {
+	@Autowired
+	private BookRepository bookRepository;
+
+	@GetMapping({"/", "/media"})
+	public String mediaMain(Model model) {
+		User user = getUser();
+		model.addAttribute("movies", movieRepository.findAll(user.getId()));
+		model.addAttribute("tvShows", showRepository.findAllByUserId(user.getId()));
+		model.addAttribute("books", bookRepository.findAll(user.getId()));
 		return "index";
 	}
 }
